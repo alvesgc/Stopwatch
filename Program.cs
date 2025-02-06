3eccd35a6f6031f1efb3cb2b51fe891d4c54f3c2
= <=>
2025-02-05 23:52:47 -0300

    feat: implement menu

--- Program.cs
+++ Program.cs
@@ -4,10 +4,20 @@ using System.Threading;
 namespace Stopwatch {
     class Program {
         static void Main(string[] args) {
-          Start();
+         Menu();
         }
-        static void Start() {
-          int time = 10;
+        static void Menu(){
+          Console.Clear();
+          Console.WriteLine("S = Segundos => 10s = 10 segundos");
+          Console.WriteLine("M = Minuto => 1m = 1 minuto");
+          Console.WriteLine("0 = Sair");
+          Console.WriteLine("Quanto tempo deseja contar?");
+          
+          string data = Console.ReadLine().ToLower();
+          char type = char.Parse(data.Substring(data.Length -1,1));
+          int time = int.Parse(data.Substring(0, data.Length -1));
+        }
+        static void Start(int time) {
           int currentTime = 0;
          while(currentTime != time) {
             Console.Clear();
@@ -15,6 +25,9 @@ namespace Stopwatch {
             Console.WriteLine(currentTime);
             Thread.Sleep(1000);
          }
+         Console.Clear();
+         Console.WriteLine("Stopwatch finalizado!");
+         Thread.Sleep(2000);
         }
     }
 }
